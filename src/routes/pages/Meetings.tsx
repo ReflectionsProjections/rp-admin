e550eeca3483826eec65cea0a55d44f30885c1e4
seventhriver <129530873+seventhriver@users.noreply.github.com>
2025-04-12 13:38:14 -0500

    Connect meetings page to API, add QR generation
    I forgot to commit connecting to the API, oops... the meetings page now interfaces with the API and it can generate QR codes

--- src/routes/pages/Meetings.tsx
+++ src/routes/pages/Meetings.tsx
@@ -1,3 +1,4 @@
+import rpLogo from '../../assets/rp_logo.svg';
 import {
   Badge,
   Box,
@@ -14,14 +15,13 @@ import {
   ModalOverlay,
   Select,
   Stack,
-  Textarea,
   useDisclosure,
   Heading,
   Text,
   Grid,
   CardFooter,
   Flex,
-  Checkbox,
+  // Checkbox,
 } from '@chakra-ui/react';
 import { EditIcon, AddIcon } from "@chakra-ui/icons";
 import moment from 'moment-timezone';
@@ -29,17 +29,18 @@ import axios from "axios";
 import { Config } from "../../config.ts";
 import React, { useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
+import { QRCode } from "react-qrcode-logo";
 
 const readable = "MMMM Do YYYY, h:mm a";
 
 const jwt = localStorage.getItem("jwt");
 
 const enum Team {
-  Full = "FULL TEAM",
-  Dev = "DEV",
-  Design = "DESIGN",
   Content = "CONTENT",
   Corporate = "CORPORATE",
+  Design = "DESIGN",
+  Dev = "DEV",
+  Full = "FULL TEAM",
   Marketing = "MARKETING",
   Ops = "OPERATIONS"
 }
@@ -70,77 +71,39 @@ function Meetings() {
   const [meetings, setMeetings] = React.useState([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [newMeeting, setNewMeeting] = React.useState({
-    meetingId: '',
-    name: '',
-    team: Team.Full,
-    time: '',
-    description: '',
-    attendees: []
+    committeeType: Team.Full,
+    startTime: '',
+    // attendees: []
   });
 
-  // const createMeeting = () => {
-  //   axios.post(Config.API_BASE_URL + "/meeting", { ...newMeeting }, {
-  //     headers: {
-  //       Authorization: jwt
-  //     }
-  //   }).then(() => {
-  //     getMeetings();
-  //     setNewMeeting({
-  //       meetingId: '',
-  //       name: '',
-  //       team: Team.Full,
-  //       time: '',
-  //       description: '',
-  //       attendees: []
-  //     });
-  //     onClose(); // Close the modal after creating the meeting
-  //   });
-  // };
-
-
-  // todo(): why am I allowed to create meetings with an invalid date (no input), but not events?
-
   const createMeeting = () => {
-    const meetingsStored = localStorage.getItem("meetings");
-    let meetings = [];
-    if (meetingsStored != null) {
-      meetings = JSON.parse(meetingsStored);
-    }
-    meetings.push(newMeeting);
-    localStorage.setItem("meetings", JSON.stringify(meetings));
-
-    getMeetings();
-    setNewMeeting({
-      meetingId: '',
-      name: '',
-      team: Team.Full,
-      time: '',
-      description: '',
-      attendees: []
+    console.log("Attempting to send meeting with committeeType: " + newMeeting.committeeType + ", startTime: " + newMeeting.startTime);
+    axios.post(Config.API_BASE_URL + "/meetings", { ...newMeeting }, {
+      headers: {
+        Authorization: jwt
+      }
+    }).then(() => {
+      getMeetings();
+      setNewMeeting({
+        committeeType: Team.Full,
+        startTime: '',
+        // attendees: []
+      });
+      onClose(); // Close the modal after creating the meeting
     });
-    onClose(); // Close the modal after creating the meeting
   };
 
-  // function getMeetings() {
-  //   axios.get(Config.API_BASE_URL + "/meetings", {
-  //     headers: {
-  //       Authorization: jwt
-  //     }
-  //   }).then(function (response) {
-  //     setMeetings(response.data);
-  //   });
-  // }
-
   function getMeetings() {
-    const meetingsStored = localStorage.getItem("meetings");
-    let meetings = [];
-    if (meetingsStored != null) {
-      meetings = JSON.parse(meetingsStored);
-    }
-    setMeetings(meetings);
+    axios.get(Config.API_BASE_URL + "/meetings", {
+      headers: {
+        Authorization: jwt
+      }
+    }).then(function (response) {
+      setMeetings(response.data);
+    });
   }
 
-  function EditModal({ meeting }: { meeting: { meetingId: string, name: string, time: string, description: string, team: Team } }) {
+  function EditModal({ meeting }: { meeting: { meetingId: string, committeeType: Team, startTime: string } }) {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [updatedValues, setUpdatedValues] = React.useState(meeting);
 
@@ -151,36 +114,20 @@ function Meetings() {
     const handleSave = () => {
       const updatedValuesUTC = {
         ...updatedValues,
-        time: moment(updatedValues.time).utc().format(),
+        startTime: moment(updatedValues.startTime).utc().format(),
       };
 
-      // const { meetingId, ...valuesWithoutMeetingId } = updatedValuesUTC;
-      // axios.put(Config.API_BASE_URL + "/meeting/" + meeting.meetingId, {
-      //   ...valuesWithoutMeetingId
-      // }, {
-      //   headers: {
-      //     Authorization: jwt
-      //   }
-      // }).then(() => {
-      //   getMeetings();
-      //   onClose();
-      // });
-
-      const meetingsStored = localStorage.getItem("meetings");
-      let meetings = [];
-      if (meetingsStored != null) {
-        meetings = JSON.parse(meetingsStored);
-      }
-      meetings as Array<{ meetingId: string }>; // typecast so we can analyze properties!
-      for (let i = 0; i < meetings.length; i++) {
-        if (meetings[i].meetingId == meeting.meetingId) {
-          console.log("meetingId " + meeting.meetingId + " found");
-          meetings[i] = updatedValuesUTC;
+      const { meetingId, ...valuesWithoutMeetingId } = updatedValuesUTC;
+      axios.put(Config.API_BASE_URL + "/meetings/" + meetingId, {
+        ...valuesWithoutMeetingId
+      }, {
+        headers: {
+          Authorization: jwt
         }
-      }
-      localStorage.setItem("meetings", JSON.stringify(meetings));
-
-      getMeetings();
+      }).then(() => {
+        getMeetings();
+        onClose();
+      });
     };
 
     return (
@@ -194,15 +141,10 @@ function Meetings() {
             <ModalHeader>Edit meeting</ModalHeader>
             <ModalCloseButton />
             <ModalBody pb={6}>
-              <Input
-                defaultValue={meeting.name}
-                mb={4}
-                onChange={(e) => setUpdatedValues({ ...updatedValues, name: e.target.value })}
-              />
               <Select
-                defaultValue={meeting.team}
+                defaultValue={meeting.committeeType}
                 mb={4}
-                onChange={(e) => setUpdatedValues({ ...updatedValues, team: e.target.value as Team })}
+                onChange={(e) => setUpdatedValues({ ...updatedValues, committeeType: e.target.value as Team })}
               >
                 <option value={Team.Full}>Full Team</option>
                 <option value={Team.Dev}>Dev</option>
@@ -214,14 +156,9 @@ function Meetings() {
               </Select>
               <Input
                 type="datetime-local"
-                defaultValue={convertToCST(meeting.time).format('yyyy-MM-DDTHH:mm')}
+                defaultValue={convertToCST(meeting.startTime).format('yyyy-MM-DDTHH:mm')}
                 mb={4}
-                onChange={(e) => setUpdatedValues({ ...updatedValues, time: moment(e.target.value).format() })}
-              />
-              <Textarea
-                defaultValue={meeting.description}
-                mb={4}
-                onChange={(e) => setUpdatedValues({ ...updatedValues, description: e.target.value })}
+                onChange={(e) => setUpdatedValues({ ...updatedValues, startTime: moment(e.target.value).format() })}
               />
               {/* <Checkbox
                 isChecked={meeting.isVisible}
@@ -229,6 +166,10 @@ function Meetings() {
               >
                 Is Visible
               </Checkbox> */}
+              <br />
+              <Stack alignItems={'center'}>
+                <Text fontStyle={'italic'}>edit meeting attendance through Attendance page</Text>
+              </Stack>
             </ModalBody>
 
             <ModalFooter>
@@ -243,66 +184,71 @@ function Meetings() {
     );
   }
 
-  // const deleteMeeting = (meetingId: string) => {
-  //   axios.delete(Config.API_BASE_URL + "/meetings/" + meetingId, {
-  //     headers: {
-  //       Authorization: jwt
-  //     }
-  //   }).then(() => {
-  //     getMeetings();
-  //   });
-  // };
-
   const deleteMeeting = (meetingId: string) => {
-    const meetingsStored = localStorage.getItem("meetings");
-    let meetings = [];
-    if (meetingsStored != null) {
-      meetings = JSON.parse(meetingsStored);
-    }
-    meetings as Array<{ meetingId: string }>; // typecast so we can analyze properties!
-    for (let i = 0; i < meetings.length; i++) {
-      if (meetings[i].meetingId == meetingId) {
-        console.log("meetingId " + meetingId + " found");
-        meetings.splice(i, 1);
+    axios.delete(Config.API_BASE_URL + "/meetings/" + meetingId, {
+      headers: {
+        Authorization: jwt
       }
-    }
-    localStorage.setItem("meetings", JSON.stringify(meetings));
-
-    getMeetings();
+    }).then(() => {
+      getMeetings();
+    });
   };
 
-  function MeetingCard({ meeting }: { meeting: { meetingId: string, name: string, time: string, description: string, team: Team } }) {
+  function MeetingCard({ meeting }: { meeting: { meetingId: string, committeeType: Team, startTime: string } }) {
     const { isOpen: isDeleteOpen, onOpen: onOpenDelete, onClose: onCloseDelete } = useDisclosure();
+    const { isOpen: isQrCodeOpen, onOpen: onOpenQrCode, onClose: onCloseQrCode } = useDisclosure();
+    const qrCodeRef = React.useRef<QRCode>(null);
 
     const confirmDelete = () => {
       deleteMeeting(meeting.meetingId);
       onCloseDelete();
     };
 
+    function copyQrCode() {
+      if (!qrCodeRef.current) {
+        return;
+      }
+      const canvas = (
+        qrCodeRef.current as unknown as {
+          canvasRef: { current: HTMLCanvasElement }
+        }
+      ).canvasRef.current;
+      canvas.toBlob((blob) => {
+        if (blob) {
+          navigator.clipboard
+            .write([new ClipboardItem({ "image/png": blob })])
+            .catch(() => {
+              alert("Failed to copy QR code.");
+            });
+        }
+      }, "image/png");
+    }
+
+    function downloadQrCode() {
+      if (!qrCodeRef.current) {
+        return;
+      }
+      qrCodeRef.current.download("png", `${meeting.committeeType + " " + convertToCST(meeting.startTime).format("MM/DD/YYYY")} QR Code.png`);
+    }
+
     return (
-      <Card maxW='sm' key={meeting.meetingId}>
+      <Card maxWidth="sm" key={meeting.meetingId}>
         <CardBody>
           {/*<Center>*/}
           {/*<Image src={event.imageUrl} alt={event.name} borderRadius='lg' />*/}
           {/*</Center>*/}
           <Stack mt='6' spacing='3'>
-            <Heading size='md'> {meeting.name}</Heading>
+            <Heading size='md'> {meeting.committeeType + " " + convertToCST(meeting.startTime).format("MM/DD/YYYY")}</Heading>
             <Badge borderRadius="full" px="2" colorScheme={
               {
-                [Team.Full]: 'green', [Team.Dev]: 'blue', [Team.Design]: 'blue',
+                [Team.Full]: 'pink', [Team.Dev]: 'blue', [Team.Design]: 'blue',
                 [Team.Content]: 'blue', [Team.Corporate]: 'blue', [Team.Marketing]: 'blue',
                 [Team.Ops]: 'blue',
-              }[meeting.team]}>
-              {meeting.team + ' !'}
+              }[meeting.committeeType]}>
+              {meeting.committeeType + ' !'}
             </Badge>
             <Text>
-              {convertToCST(meeting.time).format(readable)}
-            </Text>
-            <i> {/* todo: implement ids proper */}
-              {"id- " + meeting.meetingId + " -id"}
-            </i>
-            <Text>
-              {meeting.description}
+              {convertToCST(meeting.startTime).format(readable)}
             </Text>
           </Stack>
         </CardBody>
@@ -310,11 +256,32 @@ function Meetings() {
         <CardFooter>
           <Flex justifyContent="space-between" width="100%">
             <EditModal meeting={meeting} />
+            <Button colorScheme='gray' mx="5" onClick={onOpenQrCode}>
+              QR
+            </Button>
             <Button colorScheme='red' onClick={onOpenDelete} isDisabled={!canDelete()}>
               Delete
             </Button>
           </Flex>
         </CardFooter>
+        {/* QR CODE MODAL */}
+        <Modal isOpen={isQrCodeOpen} onClose={onCloseQrCode} isCentered>
+          <ModalOverlay backdropFilter="blur(10px)" />
+          <ModalContent alignItems="center" onClick={onCloseQrCode}>
+            <ModalHeader>Attendance: {meeting.committeeType + " " + convertToCST(meeting.startTime).format("MM/DD/YYYY")}</ModalHeader>
+            <QRCode ref={qrCodeRef} logoImage={rpLogo} logoPadding={0.05} logoPaddingStyle='circle' value={meeting.meetingId}
+              size={window.innerWidth > 400 ? window.innerWidth * 0.25 : window.innerWidth * 0.9} />
+            <Flex justifyContent="center" gap="5" width="100%">
+              <Button colorScheme="blue" m="5" onClick={copyQrCode}>
+                Copy
+              </Button>
+              <Button colorScheme="blue" m="5" onClick={downloadQrCode}>
+                Download
+              </Button>
+            </Flex>
+          </ModalContent>
+        </Modal>
+        {/* DELETE MEETING MODAL */}
         <Modal isOpen={isDeleteOpen} onClose={onCloseDelete}>
           <ModalOverlay />
           <ModalContent>
@@ -337,17 +304,6 @@ function Meetings() {
     );
   }
 
-  const addDefaultMeetings = () => {
-    const meetings = [];
-    const datetime = moment(new Date()).utc().format();
-    meetings.push({ meetingId: 'Test Meeting', name: 'Test Meeting', team: Team.Full, time: datetime, description: 'blah blah blah fill space for line wrap blah blah blah', attendees: [] });
-    meetings.push({ meetingId: 'Example', name: 'Example', team: Team.Design, time: datetime, description: 'shoutout to design qfuoihe wqeofiuq wefgoqe fiuqweflgqef', attendees: [] });
-    meetings.push({ meetingId: 'Warning', name: 'Warning', team: Team.Dev, time: datetime, description: 'These are not linked to the database! They are in localStorage', attendees: [] });
-
-    localStorage.setItem("meetings", JSON.stringify(meetings));
-    getMeetings();
-  };
-
   useEffect(() => {
     getMeetings();
   }, []); // load in meetings on load
@@ -356,7 +312,7 @@ function Meetings() {
   return (
     <Box flex="1" minW='90vw' p={4}>
       <Flex justifyContent="center" alignItems="center">
-        <Heading size="lg" onClick={addDefaultMeetings}>Meetings</Heading>
+        <Heading size="lg">Meetings</Heading>
       </Flex>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
@@ -364,15 +320,9 @@ function Meetings() {
           <ModalHeader>Create a new meeting</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            <Input
-              value={newMeeting.name}
-              onChange={(e) => setNewMeeting({ ...newMeeting, name: e.target.value, meetingId: e.target.value })}
-              placeholder="Meeting title"
-              mb={4}
-            />
             <Select
-              value={newMeeting.team}
-              onChange={(e) => setNewMeeting({ ...newMeeting, team: e.target.value as Team })}
+              value={newMeeting.committeeType}
+              onChange={(e) => setNewMeeting({ ...newMeeting, committeeType: e.target.value as Team })}
               mb={4}
             >
               <option value={Team.Full}>Full Team</option>
@@ -385,14 +335,8 @@ function Meetings() {
             </Select>
             <Input
               type="datetime-local"
-              value={newMeeting.time}
-              onChange={(e) => setNewMeeting({ ...newMeeting, time: e.target.value })}
-              mb={4}
-            />
-            <Textarea
-              value={newMeeting.description}
-              onChange={(e) => setNewMeeting({ ...newMeeting, description: e.target.value })}
-              placeholder="Meeting description"
+              value={newMeeting.startTime}
+              onChange={(e) => setNewMeeting({ ...newMeeting, startTime: e.target.value })}
               mb={4}
             />
             {/* <Checkbox
@@ -411,9 +355,10 @@ function Meetings() {
         </ModalContent>
       </Modal>
       <br />
-      <Grid templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
+      <Grid templateColumns={{ base: "repeat(1, fr)", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
         justifyItems='center' gap={6}>
-        {meetings.map((meeting: { meetingId: string, name: string, time: string, description: string, team: Team, attendees: [] }) => <MeetingCard meeting={meeting} key={meeting.meetingId} />)}
+        {/* accesses `meetings` object */}
+        {meetings.map((meeting: { meetingId: string, committeeType: Team, startTime: string }) => <MeetingCard meeting={meeting} key={meeting.meetingId} />)}
       </Grid>
       <Button
         onClick={onOpen}
@@ -435,3 +380,4 @@ function Meetings() {
 }
 
 export default Meetings;
+
